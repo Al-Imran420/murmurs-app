8107d13083d72b231ab03945e80f35485025f96d
Al-Imran420 <imran.islam070@gmail.com>
2021-11-25 18:35:01 +0600

    push beck-end implementation

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
-    <h1>You are not loged in</h1>
-    {{ data }}
+  <div class="welcome">
+    <img src="../assets/welcome_img.jpeg" alt="">
+    <h1>Welcome to Murmur!</h1>
+    {{data}}
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 export default {
   async asyncData({ $axios }): Promise<object> {
-    const res = await $axios.$post('http://localhost:3000/api/postTest')
+    const res = await $axios.$get('http://localhost:3001/api/getuser')
     console.log(res)
     return {
       data: res,
@@ -17,5 +18,31 @@ export default {
   data() {
     return {}
   },
+  mounted(){
+    console.log("he======")
+  },
 }
 </script>
+
+<style>
+  .welcome{
+      width: 100%;
+      height: 500px;
+      background-color: #ffffff;
+      padding: 15px;
+      border-radius: 10px;
+      margin-top: 12px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+  }
+  .welcome img{
+        max-width: 200px;
+  }
+  .welcome h1{
+        font-size: 46px;
+    font-weight: 500;
+    color: #001922;
+  }
+</style>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,31 +1,74 @@
 <template>
     <div class="container_right">
-        <div class="form_area">
-            <h1>Create Your Account </h1>
-            <p>Please provide your valid email address for create your account, because <br>
-                your email address will be use for your user name.</p>
-            <div class="form_input">
-                <input type="text" placeholder="Email">
-            </div>
-            <div class="form_input">
-                <input placeholder="Password">
-            </div>
-            <button>Sign In</button>
+        <div class="form_area" v-if="!current_user">
+            <form @submit.prevent="submit">
+              <h1>Create Your Account </h1>
+              <div class="form_input">
+                  <input v-model="email" placeholder="Email">
+              </div>
+              <div class="form_input">
+                  <input v-model="password" placeholder="Password">
+              </div>
+              <button type="submit">Sign In</button>
+            </form>
+        </div>
+        <div v-if="current_user">
+          <h1>You are loged in</h1>
         </div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import Vue from 'vue'
+  import VueCookies from 'vue-cookies'
+  Vue.use(VueCookies);
+  export default Vue.extend({
+    name: "login",
+    data(){
+      return{
+        test: [],
+        email:'',
+        password:'',
+        current_user:"test@gmail.com"
+      }
+    },
+    methods:{
+      async submit(){
+        let body = JSON.stringify({
+          email: this.email,
+          password: this.password,
+          logedin: true
+        })
+        this.$cookies.set("user", body , "15MIN")
+        console.log(body)
+        this.email=''
+        this.password=''
+        await this.$router.push('/home')
+      },
+      async show(){
+        console.log(this.$cookies.get("user"))
+      }
+    },
+    mounted(){
+      let currentUser = this.$cookies.get("user")
+      this.current_user = currentUser?.email
+      console.log(currentUser)
+    }
+  })
+</script>
 
 <style>
   .container_right{
     width: 100%;
     height: 100%;
-    background-color: #F8F8F8;
+    background-color: #ffffff;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    margin-top: 12px;
+    border-radius: 10px;
+    padding: 50px 15px;
   }
   .form_area{
     width: 100%;
